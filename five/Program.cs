2e9545eaa368f622ed6f03b714c2af1545e464ed
TatShar <tvku@yandex.ru>
2022-08-10 16:02:53 +0300

    vetvlenie

---
+++ five/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Привет, Машуля");
+}
+else {
+    Console.WriteLine("Привет ");
+    Console.WriteLine(username);
+}
